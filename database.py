e835d55055b47073ece7ef3765be453c26fcb2fb
hafss14 <66989571+hafss14@users.noreply.github.com>
2023-06-22 19:04:11 +0000

    Applications Successfully getting stored in Database

--- database.py
+++ database.py
@@ -46,16 +46,13 @@ def add_application_to_db(job_id, data):
       "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
     )
 
-    try:
-      conn.execute(query,
-                   job_id=job_id,
-                   full_name=data['full_name'],
-                   email=data['email'],
-                   linkedin_url=data['linkedin_url'],
-                   education=data['education'],
-                   work_experience=data['work_experience'],
-                   resume_url=data['resume_url'])
-      return True
-    except Exception as e:
-      print("Error occurred while adding application to the database:", str(e))
-      return False
+    conn.execute(
+      query, {
+        'job_id': job_id,
+        'full_name': data['full_name'],
+        'email': data['email'],
+        'linkedin_url': data['linkedin_url'],
+        'education': data['education'],
+        'work_experience': data['work_experience'],
+        'resume_url': data['resume_url']
+      })
